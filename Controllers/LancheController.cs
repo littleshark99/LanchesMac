1385c2338d8d866c0d0f423dab626539dc45c0e5
littleshark99 <rafael.clare@hotmail.com>
2025-03-06 21:00:48 -0300

    criação de view de lanche modo list e correção lanchecontroller

---
+++ Controllers/LancheController.cs
@@ -0,0 +1,21 @@
+﻿using LancheMac.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LancheMac.Controllers
+{
+    public class LancheController : Controller
+    {
+
+        private readonly ILancheRepository _lancheRepository;
+        public LancheController(ILancheRepository lancheRepository)
+        {
+            _lancheRepository = lancheRepository;
+        }
+
+        public IActionResult List()
+        {
+            var lanches = _lancheRepository.Lanche;
+            return View(lanches);
+        }
+    }
+}
